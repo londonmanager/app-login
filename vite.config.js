8cb01bb4eb36d946fcce072e65f34594d2420e0a
arianf30 <arianfernandez@gmail.com>
2024-03-18 01:35:31 -0300

    build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const BASE_URL = 'https://d1k4iglofuulip.cloudfront.net/operate'
+const BASE_URL =
+  'https://londonmanager-frontend-app.s3.sa-east-1.amazonaws.com/app'
 const BASE_URL_LOCAL = 'http://localhost:5173'
 
 export default defineConfig({
